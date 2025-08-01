58bc376a9e26d0dc296942d48134cdff03de491c
0x1eef <0x1eef@protonmail.com>
2025-08-01 18:56:56 +0000

    Include functions in "help" output

--- lib/llm/shell/commands/help.rb
+++ lib/llm/shell/commands/help.rb
@@ -19,15 +19,21 @@ class LLM::Shell::Command
     def call
       io.rewind.print(Paint["Commands", :bold, :underline]).end.end
       commands.each.with_index(1) do |command, index|
-        io.rewind.print(command_name(command, index)).end
+        io.rewind.print(command_name(command, index, :red)).end
         io.rewind.print(command_desc(command)).end.end
+      end
+      io.rewind.print(Paint["Functions", :bold, :underline]).end.end
+      functions.each.with_index(1) do |fn, index|
+        io.rewind.print(command_name(fn, index, :green)).end
+        io.rewind.print(command_desc(fn)).end.end
       end
     end
 
     private
 
     def commands = LLM.commands.values.sort_by(&:name)
-    def command_name(command, index) = [Paint[" #{index} ", :white, :red, :bold], " ", Paint[command.name, :bold]].join
+    def functions = LLM.functions.values.sort_by(&:name)
+    def command_name(command, index, bgcolor) = [Paint[" #{index} ", :white, bgcolor, :bold], " ", Paint[command.name, :bold]].join
     def command_desc(command) = command.description || "No description"
 
     LLM.command "help" do |cmd|
