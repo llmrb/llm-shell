2391eb829cdb9a373da188675781b3e6c16c58e0
0x1eef <0x1eef@protonmail.com>
2025-08-07 00:37:23 +0000

    Reduce repetition in the Help command

--- lib/llm/shell/commands/help.rb
+++ lib/llm/shell/commands/help.rb
@@ -28,43 +28,34 @@ class LLM::Shell::Command
     def render_commands(io)
       io.print(Paint["Commands", :bold, :underline], "\n\n")
       io.print(Paint["Builtin", :bold], "\n\n")
-      render_command_subset commands.select(&:builtin?), io
+      render_group commands.select(&:builtin?), io, :cyan
       io.print(Paint["User", :bold], "\n\n")
-      render_command_subset commands.reject(&:builtin?), io
-      if commands.reject(&:builtin?).empty?
-        io.print(Paint["No user commands available", :yellow], "\n\n")
-      end
+      render_group commands.reject(&:builtin?), io, :cyan
     end
 
     def render_functions(io)
       io.print(Paint["Functions", :bold, :underline], "\n\n")
       io.print(Paint["Builtin", :bold], "\n\n")
-      render_function_subset functions.select(&:builtin?), io
+      render_group functions.select(&:builtin?), io, :blue
       io.print(Paint["User", :bold], "\n\n")
-      render_function_subset functions.reject(&:builtin?), io
-      if functions.reject(&:builtin?).empty?
-        io.print(Paint["No user functions available", :yellow], "\n\n")
-      end
+      render_group functions.reject(&:builtin?), io, :blue
     end
 
-    def render_command_subset(commands, io)
-      commands.each.with_index(1) do |command, index|
-        io.print(command_name(command, index, :cyan), "\n")
-        io.print(command_desc(command), "\n\n")
-      end
-    end
-
-    def render_function_subset(functions, io)
-      functions.each.with_index(1) do |function, index|
-        io.print(command_name(function, index, :blue), "\n")
-        io.print(command_desc(function), "\n\n")
+    def render_group(commands, io, bgcolor)
+      if commands.empty?
+        io.print(Paint["None available", :yellow], "\n\n")
+      else
+        commands.each.with_index(1) do |command, index|
+          io.print(name(command, index, bgcolor), "\n")
+          io.print(desc(command), "\n\n")
+        end
       end
     end
 
     def commands = LLM.commands.values.sort_by(&:name)
     def functions = LLM.functions.values.sort_by(&:name)
-    def command_name(command, index, bgcolor) = [Paint[" #{index} ", :white, bgcolor, :bold], " ", Paint[command.name, :bold]].join
-    def command_desc(command) = command.description || "No description"
+    def name(command, index, bgcolor) = [Paint[" #{index} ", :white, bgcolor, :bold], " ", Paint[command.name, :bold]].join
+    def desc(command) = command.description || "No description"
 
     LLM.command "help" do |cmd|
       cmd.description "Show the help menu"
