26f88632ae9212bc02518edc3a07a286a7bbf832
0x1eef <0x1eef@protonmail.com>
2025-10-24 15:13:43 +0000

    Add System tool

---
+++ lib/llm/shell/tools/system.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module LLM::Shell::Tools
+  class System < LLM::Tool
+    name "system"
+    description "Run a system command"
+    param :command, String, "The command to execute", required: true
+
+    def call(command:)
+      ro, wo = IO.pipe
+      re, we = IO.pipe
+      Process.wait Process.spawn(command, out: wo, err: we)
+      [wo,we].each(&:close)
+      {stderr: re.read, stdout: ro.read}
+    end
+  end
+end
