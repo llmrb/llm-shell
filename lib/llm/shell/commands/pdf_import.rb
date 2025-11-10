b0814273a26c331567c23d7fef0625b40fcbe993
0x1eef <0x1eef@protonmail.com>
2025-11-10 14:16:39 +0000

    Add new command: pdf-import

---
+++ lib/llm/shell/commands/pdf_import.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class LLM::Shell
+  class Command::PDFImport < Command
+    name "pdf-import"
+    description "Import a PDF"
+
+    ##
+    # Completes a path with a wildcard.
+    # @param path [String]
+    #  The path to complete.
+    # @return [Array<String>]
+    #  Returns the completed path(s)
+    def self.complete(path)
+      Dir["#{path}*.pdf"]
+    end
+
+    ##
+    # Imports one or more globbed files.
+    # @return [void]
+    def call(pdf)
+      bot.chat bot.local_file(pdf)
+    end
+  end
+end
